21ca7fd177d8fd112a237b202ca2fb6b7075826a
ganboing <ganboing@gmail.com>
2015-01-31 16:49:02 +0800

    purge

--- conf/patch.sh
+++ conf/patch.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 dir=$(dirname "$0")
-echo "patching svn workspace"
-svn patch $dir/bochs_patch.diff $dir/../../
 echo "patching vc project configuration"
 patch --binary $dir/../../vs2013/bx_debug.vcxproj $dir/bx_debug.vcxproj.diff
