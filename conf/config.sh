2ed8f87715dc3fcbd277a7189a048911ad895f88
ganboing <ganboing@gmail.com>
2015-03-28 20:00:47 +0800

    mod sh files

--- conf/config.sh
+++ conf/config.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 dir=$(dirname "$0")
 cd $dir/../../
-./configure --target=pentium-windows --enable-sb16 --enable-ne2000 --enable-all-optimizations --enable-cpu-level=6 --enable-x86-64 --enable-vmx=2 --enable-pci --enable-clgd54xx --enable-voodoo --enable-usb --enable-usb-ohci --enable-usb-xhci --enable-es1370 --enable-e1000 --enable-show-ips --disable-readline --without-x --with-win32 --with-rfb --with-nogui --enable-debugger
+./configure --target=pentium-windows \
+--enable-sb16 \
+--enable-ne2000 \
+--enable-all-optimizations \
+--enable-cpu-level=6 \
+--enable-x86-64 \
+--enable-vmx=2 \
+--enable-pci \
+--enable-clgd54xx \
+--enable-voodoo \
+--enable-usb \
+--enable-usb-ohci \
+--enable-usb-xhci \
+--enable-es1370 \
+--enable-e1000 \
+--enable-show-ips \
+--disable-readline \
+--without-x \
+--with-win32 --with-rfb --with-nogui --enable-debugger
--- push_build.sh
+++ push_build.sh
@@ -9,8 +9,9 @@ MSBuild.exe bochs.vcxproj -p:Configuration=Debug -t:Build -maxcpucount:$NUMBER_O
 MSBuild.exe bochs.vcxproj -p:Configuration=Release -t:Build -maxcpucount:$NUMBER_OF_PROCESSORS
 popd
 echo "pushing to sf"
+bgsign.sh $dir/../obj-debug/bochs.exe
+bgsign.sh $dir/../obj-release/bochs.exe
 scp $dir/../obj-debug/bochs.exe $remote:$remotedir/Debug/bochs.svn-$svnrevision.exe
 scp $dir/../obj-debug/bochs.pdb $remote:$remotedir/Debug/bochs.svn-$svnrevision.pdb
 scp $dir/../obj-release/bochs.exe $remote:$remotedir/Release/bochs.svn-$svnrevision.exe
 scp $dir/../obj-release/bochs.pdb $remote:$remotedir/Release/bochs.svn-$svnrevision.pdb
-
