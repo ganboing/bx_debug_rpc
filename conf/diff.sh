bd8b812de11a1f6968ea2a9694991023586591e5
ganboing <ganboing@gmail.com>
2015-03-31 11:58:14 +0800

    modify sh and patch

---
+++ conf/diff.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+dir=$(dirname "$0")
+echo "diffing vc project configuration"
+unzip.exe $dir/../../build/win32/vs2013ex-workspace.zip -d $dir/vs2013_wk_tmp
+diff -ur $dir/vs2013_wk_tmp/vs2013/ $dir/../../vs2013/ | grep --binary -v "^Only" > $dir/vcproj.patch
+rm -rf $dir/vs2013_wk_tmp
--- conf/patch.sh
+++ conf/patch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dir=$(dirname "$0")
 echo "patching vc project configuration"
-patch --binary $dir/../../vs2013/bx_debug.vcxproj $dir/bx_debug.vcxproj.diff
+patch -d $dir/../../vs2013 --binary -i ../bx_debug_rpc/conf/vcproj.diff
