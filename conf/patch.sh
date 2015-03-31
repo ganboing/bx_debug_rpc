c0ac1da6dfbcc5b0f74c95997252868e4d549532
ganboing <ganboing@gmail.com>
2015-03-31 12:04:26 +0800

    mod

--- conf/patch.sh
+++ conf/patch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dir=$(dirname "$0")
 echo "patching vc project configuration"
-patch -d $dir/../../vs2013 --binary -i ../bx_debug_rpc/conf/vcproj.diff
+patch -d $dir/../../vs2013 --binary -i ../bx_debug_rpc/conf/vcproj.patch
